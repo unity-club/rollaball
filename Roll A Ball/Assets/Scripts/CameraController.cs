2c273859fb8938388508a3b0c775b1761f65b030
rbjacob101 <rbjacob101@gmail.com>
2020-02-20 20:19:38 -0700

    3rd tutorial complete: Moving the camera

---
+++ Roll A Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Here we get the constant offset between the camera and the player.
+		// Transform is a reference to the transform component on the player that
+		// stores information about the player's position, rotation, etc...
+		offset = transform.position - player.transform.position;
+    }
+
+	private void LateUpdate()
+	{
+		// We place the camera movement code in LateUpdate() because by the time that
+		// LateUpdate() runs, all movement calculations for this GameObject will have finished
+		transform.position = player.transform.position + offset;
+	}
+}
